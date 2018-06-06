058e8df637aabe28b615657152d97adab342db84
Jeongjin Kim <thefuturecreator@gmail.com>
2018-06-06 11:26:38 +0900

    custom login page 구현중

--- src/main/java/cothe/controllers/WebController.java
+++ src/main/java/cothe/controllers/WebController.java
@@ -1,4 +1,12 @@
 package cothe.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
+    @GetMapping("/loginPage")
+    public String login() {
+        return "login.jsp";
+    }
 }
